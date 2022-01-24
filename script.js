0bd54dae5f8fcc37808e72c90f6daa26c6548e7c
Manucho40 <rickypaine40@gmail.com>
2022-01-24 11:51:30 +0000

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
  const img = document.getElementById('img');
+ const but = document.getElementById('but');
 const varImg = 'https://api.giphy.com/v1/gifs/trending?api_key=SMROQVzzXdjKlBRbLT8KD4Zeuf6SuAxf&limit=25&rating=g';
 function fetchImg (){
     fetch(varImg)
         .then(res => res.json())
         .then(data => {
-            imgRand(data)
+            but.addEventListener('click', imgRand(data))   
             // console.log(data.data)
         })
         .catch((err) => console.log("error"))
@@ -15,11 +16,8 @@ fetchImg()
 
 
 const imgRand = (tab) => {
-    setInterval(() => {
         let randomNumb = Math.round(Math.random() * ((tab.data.length - 1) - 0) + 0);
-        console.log(randomNumb)
-        img.setAttribute('src', tab.data[randomNumb].images.downsized.url)
-    }, 1000)
+        img.setAttribute('src', tab.data[randomNumb].images.downsized.url);
     // console.log(randomNumb)
     // console.log(tab.data[randomNumb].images.downsized.url)
     
